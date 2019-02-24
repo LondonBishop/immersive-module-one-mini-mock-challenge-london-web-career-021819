8b489d7a4b49420c31fe633707d0f84e1d37e545
LondonBishop <londonbishop@codecutters.co.uk>
2019-02-24 17:46:38 +0000

    cut 1

--- author.rb
+++ author.rb
@@ -1,1 +1,60 @@
 
+class Author
+
+attr_reader :books, :name
+
+@@authors = []
+
+  def initialize(name)
+    @name = name
+    @@author << self  
+  end
+
+  def self.all
+    @@authors
+  end
+
+  def books
+      Book.all.select { |book| book.author == self}
+  end
+
+  def write_book(title, wordcount)
+     book = Book.new(self, title, wordcount)
+     @books << book
+  end
+
+  def total_words
+      totalwords = 0
+
+      Book.all.map { |book| book.author == self
+          totalwords += book.wordcount
+        }
+  end
+
+  # - `Author.most_words`
+  # should return the author instance who has written the most words
+    def self.most_words
+
+    end
+
+# ### `Author`
+# Build the following methods on the Author class
+
+# - `Author.all`
+# should return all of the authors
+
+
+# - `Author#books`
+# should return all of the books an author has written
+
+
+# - `Author#write_book`
+# should take arguments of a title and word count and make a new Book instance associated with this author
+
+
+# - `Author#total_words`
+# should return the total number of words that author has written across all of their authored books.
+
+
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,9 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+a1 = Author.new("Harry Superwriter")
+a1.write_book(a1, "Ruby at its best", 650)
+a1.write_book(a1, "life without code", 100)
+
+
 binding.pry
-
-
